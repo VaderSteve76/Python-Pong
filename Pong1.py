66f9eae753ed347b3d486ac5b7cf925a1ea458fb
VaderSteve76 <alverson.s@yahoo.com>
2019-05-29 22:49:00 -0400

    Ball

--- Pong1.py
+++ Pong1.py
@@ -28,3 +28,13 @@ paddle_b.color('white')
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape('square')
+ball.color('white')
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
